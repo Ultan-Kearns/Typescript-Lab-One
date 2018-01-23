843f7bad4458d674fbba525cc81382e02d370b40
Ultan <ultankearns@gmail.com>
2018-01-23 16:52:51 +0000

    types in typescript

---
+++ Lab1Types.ts
@@ -0,0 +1,11 @@
+//Basic types in Typescript
+let num:number = 5;
+let firstName: string = 'Ultan';
+let bool:boolean = true;
+let list:number[] = [1,2,3];
+let tuple:[String,number] = ["hello", 1];
+enum color {red,green,blue};
+let notsure:any = "a";
+let n:null = null;
+let u:undefined = undefined;
+let v:void = undefined;
